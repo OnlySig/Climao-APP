6722e3d66d3bf290e449ab7188c73993d8221302
OnlySig <gabriel.castro.pereira11@gmail.com>
2025-05-05 21:34:04 -0300

    feat: add font and lp page

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,18 @@
+import { Ionicons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
-import { StyleSheet } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity } from "react-native";
 
 export default function Index() {
   return (
-    <LinearGradient
-      colors={["#00457D", "#05051F"]}
-      style={styles.container}
-    ></LinearGradient>
+    <LinearGradient colors={["#00457D", "#05051F"]} style={styles.container}>
+      <Image source={require("../assets/images/LogoClima.png")} />
+      <Image source={require("../assets/images/lpImage.png")} />
+      <Text style={styles.title}>Boas-vindas!</Text>
+      <TouchableOpacity style={styles.btnJoin}>
+        <Text style={styles.txtJoin}>Entrar</Text>
+        <Ionicons name="arrow-forward" size={20} color={"#01080E"} />
+      </TouchableOpacity>
+    </LinearGradient>
   );
 }
 
@@ -15,5 +21,28 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    gap: 64,
+    padding: 32,
+  },
+  title: {
+    fontSize: 25,
+    color: "#fff",
+    fontFamily: "Montserrat_400Regular",
+  },
+  btnJoin: {
+    backgroundColor: "#7693FF",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    gap: 8,
+    paddingVertical: 8,
+    borderRadius: 32,
+  },
+  txtJoin: {
+    color: "#01080E",
+    fontSize: 20,
+    fontWeight: 600,
+    fontFamily: "Montserrat_600SemiBold",
   },
 });
